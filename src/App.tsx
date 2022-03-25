57aae51690fcaa52e20e885e28b0636fd5716444
fifteenmania <ktthee1995@gmail.com>
2022-03-25 18:23:32 +0900

    feat: add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import { Box, Paper } from '@mui/material';
 import './App.css';
+import MemoEditor from './MemoEditor/MemoEditor';
+
+function MemoListWrapper() {
+  return <Box sx={{
+    gridArea: 'memo-list'
+  }}>
+    MemoList
+  </Box>
+}
+
+function MemoEditorWrapper() {
+  return <Box sx={{
+    gridArea: 'memo'
+  }}>
+    <MemoEditor/>
+  </Box>
+}
+
+function HeaderWrapper() {
+  return <Box sx={{
+    gridArea: 'header'
+  }}>
+    header
+  </Box>
+}
 
 function App() {
-  const date = new Date()
   return (
-    <div>
-      <h1>{date.toTimeString()}</h1>
-    </div>
+    <Box sx={{
+      height: '100%',
+      display: 'grid',
+      gridTemplateColumns: 'repeat(4, 1fr)',
+      gridTemplateRows: 'auto',
+      gridTemplateAreas: `
+        "header header header header"
+        "memo-list memo-list memo memo"
+      `
+    }}>
+      <HeaderWrapper/>
+      <MemoListWrapper/>
+      <MemoEditorWrapper/>
+    </Box>
   );
 }
 
---
+++ src/MemoEditor/MemoEditor.tsx
@@ -0,0 +1,9 @@
+import { Box, Paper } from '@mui/material';
+
+export default function MemoEditor() {
+    return <Box>
+        <Paper elevation={3}>
+
+        </Paper>
+    </Box>
+}
